4e2f25e9c1fe756862160029c9a79af7f8bb7824
nisauex <113558933+nisauex@users.noreply.github.com>
2022-09-18 15:12:03 +0000

    Add files via upload

---
+++ ejemplo1/ejemplo1.h
@@ -0,0 +1,30 @@
+#ifndef ejemplo1_H
+#define ejemplo1_H
+
+#include <QtGui>
+#include "ui_counterDlg.h"
+#include <QTimer>
+#include <chrono>
+
+class ejemplo1 : public QWidget, public Ui_Counter
+{
+    Q_OBJECT
+    public:
+        ejemplo1();
+
+    public slots:
+        void doButton();
+        //void startButton();
+        void stopButton();
+     
+        
+    private:
+        QTimer mytimer;
+        int cont = 0;
+        
+        void cuenta();
+		
+		int trick = 5;
+};
+
+#endif // ejemplo1_H
