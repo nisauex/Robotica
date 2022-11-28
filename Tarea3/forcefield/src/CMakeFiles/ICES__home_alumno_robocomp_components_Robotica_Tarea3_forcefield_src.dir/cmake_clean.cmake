efd00144546d25836eb96888589903e970e62467
nisauex <113558933+nisauex@users.noreply.github.com>
2022-11-28 19:51:24 +0000

    Add files via upload

---
+++ Tarea3/forcefield/src/CMakeFiles/ICES__home_alumno_robocomp_components_Robotica_Tarea3_forcefield_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_alumno_robocomp_components_Robotica_Tarea3_forcefield_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
